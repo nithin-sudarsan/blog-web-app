cfabab06db8865493ee46fab80957bf88019d98c
nithin-sudarsan <nithinsudarsan740@gmail.com>
2023-05-18 12:20:08 +0530

    Changed color and added link to page title

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>My Blog</h1>
+            <Link to="/"><h1>My Blog</h1></Link>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create" style={{
                     color: "white",
-                    backgroundColor: "crimson",
+                    backgroundColor: "royalblue",
                     borderRadius: '8px'
                 }}>New Blog</Link>
             </div>
